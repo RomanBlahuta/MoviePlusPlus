bbfbbeb2fcbf3a6609aa8b9a13b93e05928ed2c2
RomanBlahuta <blahuta.r@ucu.edu.ua>
2020-06-19 05:05:51 +0300

    Movie info display on click in favourites commit

--- src/app/favlist/favlist.component.ts
+++ src/app/favlist/favlist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {objectKeys} from 'codelyzer/util/objectKeys';
+import {getJSONFromAPI} from '../../assets/helper-code/get-data-from-api';
 
 @Component({
   selector: 'app-favlist',
@@ -10,6 +11,17 @@ export class FavlistComponent implements OnInit {
 
   faves = window.localStorage;
   keys = objectKeys(this.faves);
+  displayData = false;
+  movieObj = getJSONFromAPI('http://www.omdbapi.com/?apikey=7c2ccad9&t=Z&plot=full');
+
+  onGoBack() {
+    this.displayData = false;
+  }
+
+  onGoToMovie(key: string) {
+    this.movieObj = getJSONFromAPI('http://www.omdbapi.com/?apikey=7c2ccad9&t=' + key + '&plot=full');
+    this.displayData = true;
+  }
 
   constructor() { }
 
